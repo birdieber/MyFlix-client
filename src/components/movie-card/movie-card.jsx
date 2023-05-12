d85fed5ceeb8595a79fe5629636b9d59f11ad4cf
Jessica Slusark <slusark.jessica@gmail.com>
2023-05-13 00:40:33 +0200

    Connecting and providing data from my Movie-API
    Movie-API repository can be accessed here: https://github.com/birdieber/Movie-API/tree/main

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+// Here you import the PropTypes library
+import PropTypes from 'prop-types';
+
+
 export const MovieCard = ({ movieData, onMovieClick }) => {
     return (
       <div
@@ -9,4 +13,15 @@ export const MovieCard = ({ movieData, onMovieClick }) => {
       </div>
     );
   };
+
+  // Here is where we define all the props constraints for the MovieCard
+  MovieCard.propTypes = {
+movieData: PropTypes.shape({
+title: PropTypes.string.isRequired,
+image: PropTypes.string.isRequired,
+genre:  PropTypes.string.isRequired,
+director: PropTypes.string.isRequired
+}).isRequired,
+onMovieClick: PropTypes.func.isRequired
+};
   
